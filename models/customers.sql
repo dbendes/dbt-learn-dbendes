ccb15ccadbc184ab62d3b7d6369d844d9d58318d
David Bendes <david.bendes@birchbox.com>
2020-06-10 15:52:44 +0000

    commits are working things make sense

--- models/customers.sql
+++ models/customers.sql
@@ -4,15 +4,11 @@ with customers as (
 
 ),
 
-orders as (
+order_payments as (
 
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('stg_order_payments') }}
 
 ),
-
-payments as (
-    select * from {{ref('stg_payments')}}
-)
 
 
 customer_orders as (
@@ -22,9 +18,10 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as customer_lifetime_value
 
-    from orders
+    from order_payments
 
     group by 1
 
@@ -40,7 +37,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.customer_lifetime_value
 
     from customers
 
--- models/staging/stg_order_payments.sql
+++ models/staging/stg_order_payments.sql
@@ -15,14 +15,17 @@ order_payments as (
         orders.order_id,
         payments.payment_method,
         payments.payment_status,
-        payments.amount,
+        case 
+            when payments.payment_status = "success" then payments.amount
+            else 0
+        end as amount
         orders.customer_id,
         orders.order_status,
         orders.order_date
 
     from orders
 
-    left join order_payments using (order_id)
+    left join payments using (order_id)
 
 )
 
